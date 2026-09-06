8aa2b63b4601febbfae942c44d61e4af5131cd8e
TLLtangling <2292174209@qq.com>
2026-09-06 11:34:56 +0800

    src_modified

---
+++ src_modified/src_modified/src/mem/ruby/network/garnet/CommonTypes.hh
@@ -0,0 +1,96 @@
+/*
+ * Copyright (c) 2008 Princeton University
+ * Copyright (c) 2016 Georgia Institute of Technology
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met: redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer;
+ * redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution;
+ * neither the name of the copyright holders nor the names of its
+ * contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+
+#ifndef __MEM_RUBY_NETWORK_GARNET_0_COMMONTYPES_HH__
+#define __MEM_RUBY_NETWORK_GARNET_0_COMMONTYPES_HH__
+
+#include "mem/ruby/common/NetDest.hh"
+
+namespace gem5
+{
+
+namespace ruby
+{
+
+namespace garnet
+{
+
+// All common enums and typedefs go here
+
+enum flit_type {HEAD_, BODY_, TAIL_, HEAD_TAIL_,
+                CREDIT_, NUM_FLIT_TYPE_};
+enum VC_state_type {IDLE_, VC_AB_, ACTIVE_, NUM_VC_STATE_TYPE_};
+enum VNET_type {CTRL_VNET_, DATA_VNET_, NULL_VNET_, NUM_VNET_TYPE_};
+enum flit_stage {I_, VA_, SA_, ST_, LT_, NUM_FLIT_STAGE_};
+enum link_type { EXT_IN_, EXT_OUT_, INT_, NUM_LINK_TYPES_ };
+enum RoutingAlgorithm { TABLE_ = 0, XY_ = 1, CUSTOM_ = 2, RING_ = 3,
+                        NUM_ROUTING_ALGORITHM_};
+
+// Class of a virtual channel. 
+enum VCClass { ESCAPE_VC_, ADAPTIVE_VC_, NUM_VC_CLASS_ };
+
+struct RouteInfo
+{
+    RouteInfo()
+        : vnet(0), src_ni(0), src_router(0), dest_ni(0), dest_router(0),
+          hops_traversed(0)
+    {}
+
+    // destination format for table-based routing
+    int vnet;
+    NetDest net_dest;
+
+    // src and dest format for topology-specific routing
+    int src_ni;
+    int src_router;
+    int dest_ni;
+    int dest_router;
+    int hops_traversed;
+};
+
+#define INFINITE_ 10000
+
+#define WORMHOLE_DEPTH 16
+
+// Bubble flow-control thresholds.
+
+// Injection-side gate (NetworkInterface, VC=1): minimum number of idle ring
+// VCs across BOTH directions before the local NI may inject a packet.
+#define BUBBLE_INJECT_MIN_IDLE_VC 2
+
+// Ring-entry gate (SwitchAllocator, VC>=2): minimum number of idle VCs in a
+// SINGLE ring direction before a head may enter it, keeping >=1 idle VC.
+#define BUBBLE_ENTER_MIN_IDLE_VC 2
+
+} // namespace garnet
+} // namespace ruby
+} // namespace gem5
+
+#endif //__MEM_RUBY_NETWORK_GARNET_0_COMMONTYPES_HH__
